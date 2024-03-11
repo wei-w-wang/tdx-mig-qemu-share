6bc9b1ba8ac95e458d56edb7d2468a47294ce105
Wei Wang <wei.w.wang@intel.com>
2024-03-11 19:50:50 +0800

    migration/ram: save a private page
    If a page is a private page, calls the cgs framework API to save the
    page via the vendor specific implementation. Otherwise, send the page
    via the legacy code path, where ram_save_target_page_legacy is renamed
    to ram_save_target_page_shared to indicate the path is for sending
    shared pages.
    
    Add the cgs-private-pages counter to help users understand the total
    amount of private pages that have been sent, which could be userful when
    doing performance tuning.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- linux-headers/linux/kvm.h
+++ linux-headers/linux/kvm.h
@@ -2349,4 +2349,18 @@ struct kvm_cgm_data {
 	__u64 size; /* bytes */
 };
 
+
+#define KVM_CGM_GET_MEMORY_STATE _IOWR(KVMIO,  0xd9, struct kvm_cgm_memory_state)
+
+#define KVM_CGM_GFN_NUM_MAX	512
+struct kvm_cgm_memory_state {
+	struct kvm_cgm_data data;
+	__u16 gfn_num;
+	__u8  pad1[6];
+	union {
+		__u64 gfns_uaddr;
+		__u8  pad2[8];
+	};
+};
+
 #endif /* __LINUX_KVM_H */
