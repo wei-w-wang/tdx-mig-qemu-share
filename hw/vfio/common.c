intel-staging/qemu-tdx

hw/vfio: skip static IOMMU mapping if restricted memory region
The DMA mapping of restricted memory region is manipulated in
convert_mem_attr() memory listener dynamically. It is incompatible to
map/unmap the whole range statically and pin all pages. Skip the
restricted memory region when adding static IOMMU mappings in vfio.

TODO: current UPM implementation sets the memory attribute default to
shared in KVM. To corresponding to it, VFIO is supposed to DMA map all
pages. But in TD guest, the pages will be accepted to private and
converted to shared before doing DMA. These default mappings during
region_add() can be avoided. The design can be changed in future.

Signed-off-by: Chenyi Qiang <chenyi.qiang@intel.com>

hw/vfio/common.c
@@ -731,6 +731,7 @@ static bool vfio_listener_skipped_section(MemoryRegionSection *section)
     return (!memory_region_is_ram(section->mr) &&
             !memory_region_is_iommu(section->mr)) ||
            memory_region_is_protected(section->mr) ||
+           memory_region_can_be_private(section->mr) ||
            /*
             * Sizing an enabled 64-bit BAR can cause spurious mappings to
             * addresses in the upper part of the 64-bit address space.  These

wei-w-wang/tdx-mig-qemu-share

hw/vfio/common.c
@@ -251,6 +251,7 @@ static bool vfio_listener_skipped_section(MemoryRegionSection *section)
     return (!memory_region_is_ram(section->mr) &&
             !memory_region_is_iommu(section->mr)) ||
            memory_region_is_protected(section->mr) ||
+           memory_region_has_guest_memfd(section->mr) ||
            /*
             * Sizing an enabled 64-bit BAR can cause spurious mappings to
             * addresses in the upper part of the 64-bit address space.  These