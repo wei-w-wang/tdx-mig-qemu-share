69def8bcf9f3d15542b1e03917369529433263ed
Wei Wang <wei.w.wang@intel.com>
2024-11-19 09:49:57 +0200

    i386/tdx: don't finalize for the migration destination TD
    During TD live migration, the destination TD is created to receive state
    from the source side. The TDX module will set the TD state to finalized
    after all the TD states are imported from the source. So skip sending
    the command to KVM to finalize the TD.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- target/i386/kvm/tdx.c
+++ target/i386/kvm/tdx.c
@@ -342,6 +342,14 @@ static void tdx_finalize_vm(Notifier *notifier, void *unused)
 
     tdx_post_init_vcpus();
 
+    /*
+     * Don't finalize for the migration destination TD.
+     * It will be finalzed after all the TD states successfully imported.
+     */
+    if (runstate_check(RUN_STATE_INMIGRATE)) {
+        return;
+    }
+
     for_each_tdx_fw_entry(tdvf, entry) {
         struct kvm_tdx_init_mem_region region;
         uint32_t flags;
